62db2792674814013b3781831b5b11cc50e56f7b
mantorsss <113217475+torrare06@users.noreply.github.com>
2025-11-06 04:44:12 +0300

    Add export functionality for reports

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:44:06.992Z
-Commit: Patch API endpoint error handling
+Timestamp: 2025-11-06T01:44:11.049Z
+Commit: Add export functionality for reports

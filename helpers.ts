f46b752b3d4878bd7267bde9da566db4febabd3f
mantorsss <113217475+torrare06@users.noreply.github.com>
2025-11-06 04:44:54 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:44:51.856Z
-Commit: Fix race condition in async operations
+Timestamp: 2025-11-06T01:44:53.110Z
+Commit: Patch security vulnerability in authentication

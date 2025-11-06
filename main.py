0ac8b41154d21c4ca2fd416893090739d89dbb47
mantorsss <113217475+torrare06@users.noreply.github.com>
2025-11-06 04:37:47 +0300

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:37:24.434Z
-Commit: Implement user profile customization
+Timestamp: 2025-11-06T01:37:46.569Z
+Commit: Fix memory leak in event handlers

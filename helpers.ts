8081a8e49c4155c71b6bc5f5b4f09c2fa7ea6821
mantorsss <113217475+torrare06@users.noreply.github.com>
2025-11-06 04:41:27 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:41:15.499Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2025-11-06T01:41:26.499Z
+Commit: Patch API endpoint error handling

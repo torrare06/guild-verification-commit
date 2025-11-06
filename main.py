9b2e7acf343f8628a14fc8387be52e59f24e6cdd
mantorsss <113217475+torrare06@users.noreply.github.com>
2025-11-06 04:37:08 +0300

    Resolve null pointer exception in user service

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:36:56.671Z
-Commit: Implement user profile customization
+Timestamp: 2025-11-06T01:37:07.568Z
+Commit: Resolve null pointer exception in user service

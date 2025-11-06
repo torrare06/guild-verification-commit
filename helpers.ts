6601f2e52f0785fb71b1bdd31097670fe6eadf88
mantorsss <113217475+torrare06@users.noreply.github.com>
2025-11-06 04:41:00 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:40:57.206Z
-Commit: Add export functionality for reports
+Timestamp: 2025-11-06T01:40:58.705Z
+Commit: Fix broken links in documentation

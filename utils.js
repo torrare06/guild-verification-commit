8d3fd16e5bd06a8c15c95f55471286f8f593d932
mantorsss <113217475+torrare06@users.noreply.github.com>
2025-11-06 04:48:09 +0300

    Fix race condition in async operations

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:47:47.979Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2025-11-06T01:48:08.857Z
+Commit: Fix race condition in async operations

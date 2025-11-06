5fad9fb53accbd44dade8d959219746f706b3cbf
mantorsss <113217475+torrare06@users.noreply.github.com>
2025-11-06 04:48:43 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:48:34.960Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2025-11-06T01:48:42.424Z
+Commit: Fix CSS styling bug on mobile devices

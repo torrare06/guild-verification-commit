098cd670228d948580d45baeb5270ab4af9f4a49
mantorsss <113217475+torrare06@users.noreply.github.com>
2025-11-06 04:37:18 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:37:04.839Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2025-11-06T01:37:17.550Z
+Commit: Resolve null pointer exception in user service

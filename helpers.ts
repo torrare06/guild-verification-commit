8fce72d4a77d9a38de6719f0887ea3ec621c9f87
mantorsss <113217475+torrare06@users.noreply.github.com>
2025-11-06 04:41:05 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:40:58.705Z
-Commit: Fix broken links in documentation
+Timestamp: 2025-11-06T01:41:04.095Z
+Commit: Fix CSS styling bug on mobile devices

bbcd80a896090b0b2f0cb6b0dbcef78d4603fb8e
mantorsss <113217475+torrare06@users.noreply.github.com>
2025-11-06 04:48:54 +0300

    Fix memory leak in event handlers

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:48:51.228Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2025-11-06T01:48:53.610Z
+Commit: Fix memory leak in event handlers

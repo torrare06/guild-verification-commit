0b2d55318692866f7cd8ee980840b6a6b62f7052
mantorsss <113217475+torrare06@users.noreply.github.com>
2025-11-06 04:43:04 +0300

    Correct typo in configuration file

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:41:42.719Z
-Commit: Add export functionality for reports
+Timestamp: 2025-11-06T01:43:03.585Z
+Commit: Correct typo in configuration file
